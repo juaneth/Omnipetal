6156e7e1e1b882925cd8178231ca7b8d3abb4c39
Juaneth <euanwalker2008@gmail.com>
2022-04-13 00:42:36 +0100

    added storage functionality

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 const fs = require("fs");
 console.log(fs.readFileSync("./src/sign.txt", "utf8"));
+
+const Storage = require("node-storage");
+const getAppDataPath = require("appdata-path");
+
+console.log(`Config:: ${getAppDataPath()}/omnipetal/config.json`);
+let store = new Storage(`${getAppDataPath()}/omnipetal/config.json`);
