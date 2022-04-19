abd44bb982a33967e377d705a1e1fbdbfd3e9a9a
Juaneth <euanwalker2008@gmail.com>
2022-04-19 23:42:57 +0100

    added ip filling out on load and select change

--- src/manage-remotes/remote-list.js
+++ src/manage-remotes/remote-list.js
@@ -14,12 +14,26 @@ data.remotes.forEach((obj) => {
     option.className = "text-center text-white transition-all";
     remoteList.appendChild(option);
 });
+
 //  Add change event (When select changed)
 remoteList.addEventListener("change", function() {
     data.remotes.forEach((obj) => {
         let keys = Object.keys(obj);
         if (keys[0] === remoteList.value) {
-            //
+            // Fill out IP and Port fields
+            document.getElementById("ip").value = obj[keys[0]].ip;
+            document.getElementById("port").value = obj[keys[0]].port;
+        }
+    });
+});
+// Fill out ip and port fields on load
+window.addEventListener("load", function load() {
+    data.remotes.forEach((obj) => {
+        let keys = Object.keys(obj);
+        if (keys[0] === remoteList.value) {
+            // Fill out IP and Port fields
+            document.getElementById("ip").value = obj[keys[0]].ip;
+            document.getElementById("port").value = obj[keys[0]].port;
         }
     });
 });
