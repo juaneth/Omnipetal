207e9b333ff4b9d991383100a9efb130c25fb0b0
Juaneth <euanwalker2008@gmail.com>
2022-04-19 23:29:07 +0100

    Removed ping because way too difficult for something so simple

--- src/manage-remotes/remote-list.js
+++ src/manage-remotes/remote-list.js
@@ -4,7 +4,7 @@ const getAppDataPath = require("appdata-path");
 // Add remotes to remote list options
 let data = require(`${getAppDataPath()}/omnipetal/config.json`);
 
-let x = document.getElementById("remotes");
+let remoteList = document.getElementById("remotes");
 
 data.remotes.forEach((obj) => {
     let keys = Object.keys(obj);
@@ -12,27 +12,14 @@ data.remotes.forEach((obj) => {
     let option = document.createElement("option");
     option.text = keys[0];
     option.className = "text-center text-white transition-all";
-    x.appendChild(option);
+    remoteList.appendChild(option);
 });
-
-// Ping remote to get status and latency
-function pingRemote(remote) {
-    const ping = require("ping");
-
-    (async function() {
-        const result = await ping.promise.probe(remote, {
-            timeout: 10,
-        });
-
-        return parseFloat(result.avg);
-    })();
-}
-
-pingRemote();
-
-x.addEventListener("change", function() {
-    let remote = x.value;
-    let keys = data.remotes.find((item) => item == remote);
-
-    console.log(keys);
+//  Add change event (When select changed)
+remoteList.addEventListener("change", function() {
+    data.remotes.forEach((obj) => {
+        let keys = Object.keys(obj);
+        if (keys[0] === remoteList.value) {
+            //
+        }
+    });
 });
