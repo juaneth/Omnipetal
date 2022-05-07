2931985f7552cc70b359e42a14961eb89c95602b
Juaneth <euanwalker2008@gmail.com>
2022-05-07 21:44:56 +0100

    uhhhhh

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ const createWindow = () => {
         title: "Omnipetal",
         icon: path.join(__dirname, "icon.ico"),
         titleBarStyle: "hidden",
+        trafficLightPosition: "topRight",
         autoHideMenuBar: true,
         titleBarOverlay: {
             color: "#1f2937",
