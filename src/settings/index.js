6e293a59f6349a4c9e3df616bff1e1d895eb7c62
Juaneth <euanwalker2008@gmail.com>
2022-05-08 03:45:37 +0100

    added config reset button

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,24 +1,43 @@
 const fs = require("fs");
 const getAppDataPath = require("appdata-path");
 
-// Add remotes to remote list options
+// Add config
 let data = require(`${getAppDataPath()}/omnipetal/config.json`);
 
+// On load of the page, set textbox to config value
 document.getElementById('port').value = data.settings[0].defaultPort
 
-function savePort() {
+// Save Port to config
+document.getElementById('change-port').addEventListener('click', function() {
     data.settings[0].defaultPort = document.getElementById('port').value
 
     fs.writeFileSync(
         `${getAppDataPath()}/omnipetal/config.json`,
         JSON.stringify(data)
     );
-}
-
-document.getElementById('change-port').addEventListener('click', function() {
-    savePort()
 })
 
+// Version Number
 var pjson = require('../../package.json');
-
 document.getElementById('version').innerHTML = pjson.version;
+
+// Reset Config
+document.getElementById('reset-button').addEventListener('click', function() {
+    let defaultsettings = {
+        settings: [{
+            "defaultPort": "2065",
+        }, ],
+        remotes: [{
+            name: "this-device",
+            ip: "locahost",
+            port: "2065",
+        }, ],
+        keys: [{
+
+        }, ],
+    };
+
+    fs.writeFileSync(`${getAppDataPath()}/omnipetal/config.json`, JSON.stringify(defaultsettings));
+
+    location.reload();
+})
